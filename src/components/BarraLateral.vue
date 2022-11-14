578fe131b49937e1dbd53ac702e89594e64c442a
xLucaspx <xlucaspx@gmail.com>
2022-11-14 18:49:34 -0300

    Iniciando Alura Tracker

---
+++ src/components/BarraLateral.vue
@@ -0,0 +1,32 @@
+<template>
+    <header>
+        <h1>
+            <img src="../assets/logo.png" alt="">
+        </h1>
+    </header>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'BarraLateral'
+})
+</script>
+
+<style scoped>
+/* Todo o estilo da página */
+header {
+    background: #0d3b66;
+    width: 100%;
+    height: 100vh;
+    padding: 1rem;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
---
+++ src/components/FormularioTarefa.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="box">
+        <div class="columns">
+            <div class="column is-8" role="form" aria-label="Formulário para criação de uma nova tarefa">
+                <input type="text" class="input" placeholder="Qual tarefa você deseja iniciar?">
+            </div>
+
+            <div class="column">
+                <div class="is-flex is-align-items-center is-justify-content-space-between">
+                    <section>
+                        <strong>
+                            {{ tempoDecorrido }}
+                        </strong>
+                    </section>
+
+                    <button class="button" @click="iniciar">
+                        <span class="icon">
+                            <i class="fas fa-play"></i>
+                        </span>
+                        <span>Start</span>
+                    </button>
+
+                    <button class="button" @click="finalizar">
+                        <span class="icon">
+                            <i class="fas fa-stop"></i>
+                        </span>
+                        <span>Stop</span>
+                    </button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'FormularioTarefa',
+    data() {
+        return {
+            tempoEmSegundos: 0
+        }
+    },
+    computed: {
+        tempoDecorrido () : string {
+            // formato '00:00:00'
+            return new Date(this.tempoEmSegundos * 1000).toISOString().substring(11, 19)
+        }
+    },
+    methods: {
+        iniciar() {
+            // começar a contagem
+            // 1s = 1000ms
+            setInterval(() => {
+                this.tempoEmSegundos++;
+            }, 1000)
+        },
+        finalizar() {
+            console.log("Não estou curtindo o vue cara")
+        }
+    }
+})
+</script>
